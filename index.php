7ad10972ff3c1dc00c92a1bb00e595c3cc4eb8a1
Luis Miguel Barrera <64357950+luismibaro@users.noreply.github.com>
2024-02-08 16:10:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+ 
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Mostrar Resultados</title>
+  
+</head>
+<body>
+<header>
+    <h1>Tarea Unidad 8 DWES.</h1>
+    <h2>Aplicaciones web híbridas.</h2>
+</header>
+
+<main>
+</main>
+<section>
+    <div id="caja">
+            <h1>Personajes</h1>
+            <main></main>
+            <script src="index.js"></script>
+    </div>
+</section>
+<footer>
+    <p>49030803R - Luis Miguel Barrera</p>
+</footer>
+</body>
+</html>
